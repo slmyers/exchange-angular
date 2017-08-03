f99a7cfe65d124a80feb0da06cb21df72ccb9c9a
Steven Myers <steven.myers@microquest.ca>
2017-08-03 13:40:48 -0600

    show component that is component overview

--- src/app/components-overview/components-overview.component.ts
+++ src/app/components-overview/components-overview.component.ts
@@ -1,18 +1,82 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-overview',
+  template: `
+  <main>
+    <app-side-nav></app-side-nav>
+    <section class="content">
+      <section class="code-block">
+        <prism-block [code]="typescriptCode" [language]="'css'"></prism-block>
+      </section>
+    </section>
+  </main>
+  `,
+  styles: [
+    `main {
+      width: 95vw;
+      height: auto;
+      overflow-x: hidden;
+      overflow-y: auto;
+      background-color: transparent;
+      display: flex;
+      margin: 2rem auto;
+    }
+
+    section.content {
+      margin-left: 2rem;
+      background-color: rgb(245, 245, 245);
+      width: 80vw;
+      border-radius: 4px;
+    }
+
+    section.code-block {
+      width: 75%;
+      margin: 0 auto;
+    }`
+  ],
+})
+export class ComponentsOverviewComponent {
+  typescriptCode =
+`
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-components-overview',
-  templateUrl: './components-overview.component.html',
-  styleUrls: ['./components-overview.component.css'],
+  template: \`
+    <main>
+      <app-side-nav></app-side-nav>
+      <section class="content">
+        <section class="code-block">
+          <prism-block [code]="typescriptCode" [language]="'css'"></prism-block>
+        </section>
+      </section>
+    </main>
+  \`,
+  styles: [\`
+      main {
+      width: 95vw;
+      height: auto;
+      overflow-x: hidden;
+      overflow-y: auto;
+      background-color: transparent;
+      display: flex;
+      margin: 2rem auto;
+    }
+
+    section.content {
+      margin-left: 2rem;
+      background-color: rgb(245, 245, 245);
+      width: 80vw;
+      border-radius: 4px;
+    }
+
+    section.code-block {
+        width: 75%;
+        margin: 0 auto;
+    }\`
+  ],
 })
-export class ComponentsOverviewComponent implements OnInit {
-  cssCode =
-`
-p {
-  color:red;
-}`.trim();
-  constructor() { }
-
-  ngOnInit() {}
-
+export class ComponentsOverviewComponent{ typescriptCode = \` ... \` }
+`;
 }
