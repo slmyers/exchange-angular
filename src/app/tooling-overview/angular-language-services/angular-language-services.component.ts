164823555c31b3e7a23d1a37d77b16adf2abfaa2
Steven Myers <steven.myers@microquest.ca>
2017-08-25 13:44:30 -0600

    started work on tooling section

---
+++ src/app/tooling-overview/angular-language-services/angular-language-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-language-services',
+  templateUrl: './angular-language-services.component.html',
+  styleUrls: ['./angular-language-services.component.css']
+})
+export class AngularLanguageServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
