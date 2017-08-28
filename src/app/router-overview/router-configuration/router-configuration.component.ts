25b4a6bf34268f91110e2d3d8508622ef78fdfe8
Steven Myers <steven.myers@microquest.ca>
2017-08-28 13:41:11 -0600

    router work done at lunch monday

--- src/app/router-overview/router-configuration/router-configuration.component.ts
+++ src/app/router-overview/router-configuration/router-configuration.component.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-router-configuration',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./router-configuration.component.css']
 })
 export class RouterConfigurationComponent implements OnInit {
-  routerConfiguration= `
+  routerConfiguration = `
 const appRoutes: Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
   { path: 'hero/:id',      component: HeroDetailComponent },
@@ -37,9 +38,69 @@ const appRoutes: Routes = [
 export class AppModule { }
 `;
 
-  constructor() { }
+  componentCode = `
+...
+  colors = [
+    { color: 'red' },
+    { color: 'green' },
+    { color: 'blue' }
+  ];
+
+  color: Observable<string>;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.color = this.route.queryParamMap
+      .map( (obj: any) => obj.params.color ? obj.params.color : 'inherit');
+  }
+...
+`;
+
+  templateCode = `
+<ul>
+  <li *ngFor="let choice of colors">
+    <a
+      routerLinkActive="active-link"
+      [routerLink]="['./']"
+      [queryParams]="{ color: choice.color}"
+      [routerLinkActiveOptions]="{ exact: true }">
+      {{ choice.color }} 
+    </a>
+  </li>
+  <li>
+    <a routerLinkActive="active-link" [routerLink]="['./']" [routerLinkActiveOptions]="{ exact: true }"> none </a>
+  </li>
+</ul>
+
+<div class="color-output">
+  <div 
+    *ngFor="let choice of colors" 
+    [ngStyle]="{'border-color': (color | async) === choice.color ? choice.color : null }"> 
+    {{ choice.color }}
+  </div>
+</div>
+
+<section class="bullet-points">
+  <ul title="overview points" [style.color]="color | async">
+    <li> This color is {{ color | async }} </li>
+  </ul>
+</section>
+`;
+
+  colors = [
+    { color: 'red' },
+    { color: 'green' },
+    { color: 'blue' }
+  ];
+
+  color: Observable<string>;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.color = this.route.queryParamMap
+      .map( (obj: any) => obj.params.color ? obj.params.color : 'inherit');
   }
 
 }
